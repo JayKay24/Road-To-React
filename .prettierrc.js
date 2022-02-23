951b8f22e8a35a19b77ec638428f620a7d22cc1f
Jimmy <jameskinyua590@gmail.com>
2022-02-24 00:30:13 +0300

    Remove boilerplate from <App />

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    printWidth: 100,
-    tabWidth: 2,
-    singleQuote: true,
-    semi: true,
-    trailingComma: 'all',
-    arrowParens: 'always',
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
 };
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // eslint-disable-next-line react/react-in-jsx-scope
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
