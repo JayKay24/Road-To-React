d6a0b63120de7b9624f28265cfc9c7e5a14a3738
Jimmy <jameskinyua590@gmail.com>
2022-03-01 12:55:05 +0300

    Extract out List & Search components

--- src/App.js
+++ src/App.js
@@ -24,23 +24,39 @@ function App() {
     <div>
       <h1>My Hacker stories</h1>
 
+      <Search />
+
+      <hr />
+
+      <List />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-
-      <ul>
-        {list.map((item) => {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 }
