27225685a10c04a434fc850b3956fb70e5940f86
Jimmy <jameskinyua590@gmail.com>
2022-03-06 22:32:50 +0300

    Create Item component as a child of List

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     radix: 'error',
     'space-before-blocks': 'error',
     'import/prefer-default-export': 'off',
-    'react/prop-types': 1,
+    'react/prop-types': 0,
   },
   overrides: [
     {
--- src/App.js
+++ src/App.js
@@ -36,16 +36,20 @@ const App = () => {
 const List = ({ list }) => (
   <ul>
     {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
+);
+
+const Item = ({ item }) => (
+  <li key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
 );
 
 const Search = () => {
