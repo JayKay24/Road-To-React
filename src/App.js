4b75dd3d984772a864b0a606eb9236c2d225d24b
Jimmy <jameskinyua590@gmail.com>
2022-03-22 21:13:13 +0300

    Replace enclosing div with fragment

--- src/App.js
+++ src/App.js
@@ -73,10 +73,10 @@ const Item = ({ title, url, author, num_comments, points }) => (
 );
 
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" onChange={onSearch} value={search} />
-  </div>
+  </>
 );
 
 export default App;
